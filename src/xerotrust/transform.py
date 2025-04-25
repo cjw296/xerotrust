fc0d2d370a44ede0cf04e34253abf97de6930b21
Chris Withers <chris@withers.org>
2025-04-25 18:49:14 +0100

    Refactor transforming for use in `xerotrust explore`

--- src/xerotrust/transform.py
+++ src/xerotrust/transform.py
@@ -1,8 +1,9 @@
 import json
 from datetime import datetime, timezone, date
 from pprint import pformat
-from typing import Any, Callable
+from typing import Any, Callable, TypeAlias, Sequence, Iterable
 
+Transformer: TypeAlias = Callable[[Any], Any]
 
 def itemgetter(key: str, default: Any = None) -> Callable[[dict[str, Any]], Any]:
     def getter(obj: dict[str, Any]) -> Any:
@@ -16,3 +17,15 @@ TRANSFORMERS: dict[str, Callable[[Any], Any]] = {
     'tenant': itemgetter('tenantName'),
     'pretty': pformat,
 }
+
+
+def show(
+    items: Iterable[dict[str, Any]], transforms: Sequence[str], fields: Sequence[str], newline: bool
+) -> Any:
+    if not (transforms or fields):
+        transforms = ('json',)
+    transformers = [TRANSFORMERS[t] for t in transforms]
+    transformers.extend(itemgetter(f) for f in fields)
+    sep = '\n' if newline else ' '
+    for item in items:
+        print(*(t(item) for t in transformers), sep=sep)
