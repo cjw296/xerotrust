2fb8fc42f5e5fb95ba9b7cc86312394d5ad3d8ca
Chris Withers <chris@withers.org>
2025-06-22 13:10:12 +0100

    glob order is not deterministic across platforms

--- src/xerotrust/jsonl.py
+++ src/xerotrust/jsonl.py
@@ -6,7 +6,7 @@ from typing import Iterable, Any
 def jsonl_stream(paths_or_globs: Iterable[Path | str]) -> Iterable[dict[str, Any]]:
     for path_or_glob in paths_or_globs:
         paths = [path_or_glob] if isinstance(path_or_glob, Path) else Path().glob(path_or_glob)
-        for path in paths:
+        for path in sorted(paths):
             with path.open() as source:
                 for line in source:
                     yield json.loads(line)
