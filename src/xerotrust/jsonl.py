668bff586df00520cf876e5b6510383bbbf730a5
Chris Withers <chris@withers.org>
2025-06-22 14:05:47 +0100

    Add support for non-relative globs to jsonl_stream

--- src/xerotrust/jsonl.py
+++ src/xerotrust/jsonl.py
@@ -1,11 +1,19 @@
 import json
+from decimal import Decimal
 from pathlib import Path
 from typing import Iterable, Any
 
 
 def jsonl_stream(paths_or_globs: Iterable[Path | str]) -> Iterable[dict[str, Any]]:
     for path_or_glob in paths_or_globs:
-        paths = [path_or_glob] if isinstance(path_or_glob, Path) else Path().glob(path_or_glob)
+        if isinstance(path_or_glob, Path):
+            paths = [path_or_glob]
+        else:
+            glob_path = Path(path_or_glob)
+            if glob_path.is_absolute():
+                paths = glob_path.parent.glob(glob_path.name)
+            else:
+                paths = Path().glob(path_or_glob)
         for path in sorted(paths):
             with path.open() as source:
                 for line in source:
--- tests/test_jsonl.py
+++ tests/test_jsonl.py
@@ -22,3 +22,11 @@ def test_path_and_glob(tmp_path: Path) -> None:
     c.write_text('"C"')
     with chdir(tmp_path):
         compare(jsonl_stream(['a*.jsonl', b]), expected=generator('A1', 'A2', 'B'))
+
+
+def test_non_relative(tmp_path: Path) -> None:
+    a1 = tmp_path / "a1.jsonl"
+    a2 = tmp_path / "a2.jsonl"
+    a1.write_text('"A1"')
+    a2.write_text('"A2"')
+    compare(jsonl_stream([str(tmp_path / 'a*.jsonl')]), expected=generator('A1', 'A2'))
